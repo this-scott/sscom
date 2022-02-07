fb626966e1e7bd12ac0d0583fddfa0f787bdbb42
Scott <This_Scott@outlook.com>
2022-02-06 22:55:19 -0500

    gonna break it rq

--- components/navbar.js
+++ components/navbar.js
@@ -1,42 +1,48 @@
 import Link from 'next/link';
+import useWindowDimensions from '../hooks/useWindowDimensions';
 import navstyles from './navbar.module.css';
 
 export default function Navbar({}) {
-    return (
-        <div className={navstyles.header}>
-            <div className={navstyles.lcontainer}>
-                <div className={navstyles.tlink}>
-                    <h1 className={navstyles.h1}>
-                        <Link href={"/"}>
-                            <a className={navstyles.linkw}>Scott Styslinger:</a>
-                        </Link>
-                    </h1>
-                </div>
-                <nav className={navstyles.navcontainer}>
-                    <div className={navstyles.links}/*links that are broken rn, to be fixed*/>
-                        <h2>
-                            <Link href={"/resume"}>
-                                <a className={navstyles.linkw}>Resume</a>
+    const {height, width} = useWindowDimensions();
+
+    if (width > 768) {
+        return (
+            <div className={navstyles.header}>
+                <div className={navstyles.lcontainer}>
+                    <div className={navstyles.tlink}>
+                        <h1 className={navstyles.h1}>
+                            <Link href={"/"}>
+                                <a className={navstyles.linkw}>Scott Styslinger:</a>
                             </Link>
-                        </h2>
-                        <h2>
-                            <a className={navstyles.linkw} href="https://github.com/this-scott">
-                                Projects
-                            </a>
-                        </h2>
-                        <h2>
-                            <Link href={"/cast"}>
-                                <a className={navstyles.linkw}>Cast And Crew</a>
-                            </Link>
-                        </h2>
-                        <h2>
-                            <a className={navstyles.linkw} href="https://www.linkedin.com/in/this-scott/">
-                                LinkedIn
-                            </a>
-                        </h2>
+                        </h1>
                     </div>
-                </nav>
+                    <nav className={navstyles.navcontainer}>
+                        <div className={navstyles.links}/*links that are broken rn, to be fixed*/>
+                            <h2>
+                                <Link href={"/resume"}>
+                                    <a className={navstyles.linkw}>Resume</a>
+                                </Link>
+                            </h2>
+                            <h2>
+                                <a className={navstyles.linkw} href="https://github.com/this-scott">
+                                    Projects
+                                </a>
+                            </h2>
+                            <h2>
+                                <Link href={"/cast"}>
+                                    <a className={navstyles.linkw}>Cast And Crew</a>
+                                </Link>
+                            </h2>
+                            <h2>
+                                <a className={navstyles.linkw} href="https://www.linkedin.com/in/this-scott/">
+                                    LinkedIn
+                                </a>
+                            </h2>
+                        </div>
+                    </nav>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+    
 }
