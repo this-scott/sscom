544241b56c92692c58b5790ea5df12590f649ecd
Scott <This_Scott@outlook.com>
2022-02-07 01:49:23 -0500

    Version 0.9.1

--- components/aboutme.js
+++ components/aboutme.js
@@ -4,11 +4,11 @@ export default function Aboutme(){
     return (
         <div>
             <div className={aboutme.titlediv}>
-                <h1>About Me</h1>
+                <h1>The 'About Me' will be here in v1</h1>
             </div>
             <div className={aboutme.row}>
                 <div className={aboutme.box}>
-                    <h1>The Origins</h1>
+                    <h1>Web dev sucks</h1>
                     <p></p>
                 </div>
             </div>
